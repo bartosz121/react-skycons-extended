c929200444d148cbc2b45741a3264e5be5e0d8b3
bartosz121 <bmagiera121@gmail.com>
2021-07-30 20:01:11 +0200

    Add skycons.js

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,13 @@
-import React, { FC, HTMLAttributes, ReactChild } from 'react';
+import React, { useEffect, useRef } from 'react';
 
-export interface Props extends HTMLAttributes<HTMLDivElement> {
-  /** custom content, defaults to 'the snozzberries taste like snozzberries' */
-  children?: ReactChild;
-}
+//@ts-ignore
+import { Skycons } from "./skycons/skycons"
 
-// Please do not use types off of a default export module or else Storybook Docs will suffer.
-// see: https://github.com/storybookjs/storybook/issues/9556
-/**
- * A custom Thing component. Neat!
- */
-export const Thing: FC<Props> = ({ children }) => {
-  return <div>{children || `the snozzberries taste like snozzberries`}</div>;
+export interface Props {} // TODO
+
+
+export const Thing: React.FC<Props> = ({}) => {
+  return(
+    <div></div>
+  )
 };
