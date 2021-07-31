3db9764fb3cd00b1fc6f32286c9fa976fdb85d84
bartosz121 <bmagiera121@gmail.com>
2021-07-31 15:23:35 +0200

    👀 Add basic test

---
+++ test/ReactSkycon.test.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import * as ReactDOM from 'react-dom';
+import { ReactSkycon } from '../src';
+import SkyconType from '../src/SkyconType'
+
+const props = {
+  icon: SkyconType.CLEAR_DAY,
+  color: "gold",
+  size: {width: 128, height: 128},
+  animate: true,
+  resizeClear: true
+}
+
+describe('ReactSkycon', () => {
+  it('renders without crashing', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(<ReactSkycon {...props} />, div);
+    ReactDOM.unmountComponentAtNode(div);
+  });
+});
