0a4351e6bb1e825bad2683268d59c2d8d0f118f7
bartosz121 <bmagiera121@gmail.com>
2021-07-31 14:09:11 +0200

    Move ColorableParts and SkyconType to src directory

---
+++ src/ColorableParts.ts
@@ -0,0 +1,17 @@
+interface ColorableParts {
+  sun?: string;
+  moon?: string;
+  light_cloud?: string;
+  cloud?: string;
+  dark_cloud?: string;
+  rain?: string;
+  snow?: string;
+  thunder?: string;
+  wind?: string;
+  leaf?: string;
+  hail?: string;
+  sleet?: string;
+  fog?: string;
+}
+
+export default ColorableParts;
