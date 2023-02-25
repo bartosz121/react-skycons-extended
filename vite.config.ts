ebac563e83d1b8e62b6a901a17e6cb566c9e5f42
bartosz121 <bmagiera121@gmail.com>
2023-02-25 19:16:35 +0100

    Add declaration, use dts plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { resolve } from "path";
+import dts from "vite-plugin-dts";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -20,5 +21,5 @@ export default defineConfig({
       },
     },
   },
-  plugins: [react()],
+  plugins: [react(), dts()],
 });
